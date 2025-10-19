21424959535b8419edaaf19c174abe8a60620341
MaxGrobler <groblerm226@kesbath.com>
2025-10-19 17:41:31 +0100

    allow UI to call it and print to log coords i get

--- Controllers/UavRoutesController.cs
+++ Controllers/UavRoutesController.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UavRouter.Data;
@@ -14,7 +15,7 @@ public class UavRouteController : ControllerBase
         _context = context;
     }
 
-    // GET: api/uavroutes
+    // GET endpoint
     [HttpGet]
     public async Task<ActionResult<IEnumerable<FlightRoute>>> GetUavRoutes()
     {
@@ -23,4 +24,21 @@ public class UavRouteController : ControllerBase
         var allRoutes = uav_routes.ToList();
         return allRoutes;
     }
+    [HttpPost("PlanRoute")]
+    public IActionResult PlanRoute([FromBody] JsonElement myjsonstring)
+    {
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var Points = JsonSerializer.Deserialize<WayPoint[]>(myjsonstring, options);
+
+        //expect structure:"[{\"lat\":51.37949539652476,\"lng\":-2.3742485046386723},{\"lat\":51.389565827780466,\"lng\":-2.353906631469727}]"
+        Console.WriteLine("Received waypoints:");
+        foreach (var p in Points)
+        {
+            Console.WriteLine($"Lat: {p.Latitude}, Lng: {p.Longitude}, Alt: {p.AltitudeMeters}");
+        }
+        return Ok(Points);
+
+    }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
+
+//allw my UI to call in for this port. Otherwise it gets cors problems.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowUI", policy =>
+        policy.WithOrigins("http://localhost:5056")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 
 builder.Services.AddDbContext<UAVRouterContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-
 var app = builder.Build();
-
+app.UseCors("AllowUI"); //use my "allow policy" here
 app.MapControllers();
 app.Run();
